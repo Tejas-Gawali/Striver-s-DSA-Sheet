ddd001ee33d3b4614b38c2e7f91ead56305c1882
Tejas Gawali <106678713+Tejas-Gawali@users.noreply.github.com>
2023-06-15 14:15:01 +0530

    Create searchInSorted.java

---
+++ searchInSorted.java
@@ -0,0 +1,12 @@
+class Solution{
+    static int searchInSorted(int arr[], int N, int K)
+    {
+        
+        for(int num:arr){
+            if(num == K){
+                return 1;
+            }
+        }
+        return -1;
+        
+    
